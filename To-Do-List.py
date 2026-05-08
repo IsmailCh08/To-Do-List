7743633ded3edab3214e5e2e267e6224a996c913
Ismail <Ismailchaudry123@icloud.com>
2026-05-08 09:11:40 -0500

    Basic boiler + add_task function

---
+++ To-Do-List.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+tasks = []
+tasks_dict = {}
+task = ''
+priority = ''
+status = ''
+
+if os.path.exists('tasks.json'):
+    with open('tasks.json', 'r') as file:
+        tasks = json.load(file)
+else:
+    task_List = []
+
+def add_Task():
+    task = input("What is the task? ")
+    tasks_dict["task"] = task
+
+    priority = input("Is the priority High, Medium or Low? ")
+    tasks_dict["priority"] = priority
+
+    status = input("Is the task done or not done? ")
+    tasks_dict['status'] = status
+
+    tasks.append(tasks_dict)
+    with open('task.json', 'w' ) as file:
+        json.dump(tasks, file)
+
+add_Task()
