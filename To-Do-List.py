a5fff046c9f2f889d747375a553a3b9e67af8358
Ismail <Ismailchaudry123@icloud.com>
2026-05-09 10:09:15 -0500

    Evetything works, now one to error handling

--- To-Do-List.py
+++ To-Do-List.py
@@ -13,12 +13,19 @@ else:
     task_List = []
 
 def mark_Task():
-    task = input("What tasks you want to change? ")
+    task_name = input("Which task do you want to change? ")
+    task = input("Do you want to change the task, priority or status? ")
     change_Task_To = input("What do you want to change the task to? ")
 
     for i in tasks:
-        if i["task"] == task:
-            i["status"] = change_Task_To
+        if i["task"] == task_name:
+            if task == "task":
+                i["task"] = change_Task_To
+            elif task == "priority":
+                i["priority"] = change_Task_To
+            elif task == "status":
+                i["status"] = change_Task_To
+
     with open('tasks.json', 'w' ) as file:
         json.dump(tasks, file)
 
@@ -52,8 +59,22 @@ def view_Task():
         if i["task"] == task:
             print(f'Task: {i["task"]}, Priority: {i["priority"]}, Status: {i["status"]}' )
 
-option = input("Press 1 to add a task, Press 2 to view a taks, Press 3 to mark a task, Press 4 to Delete a task ")
+option = input("Press 1 to add a task, 2 to view a taks, 3 to mark a task, 4 to Delete a task, 5 to view incomplete tasks, 6 to view priorities ")
 option = int(option)
+
+def view_incomplete():
+    for i in tasks:
+        if i["status"] == "not done":
+            print(f"Not Done: {i["task"]}")
+
+def view_priority():
+    for i in tasks:
+        if i["priority"] == "high":
+            print(f"High: {i["task"]}")
+        elif i["priority"] == "medium":
+            print(f"Medium: {i["task"]}")
+        elif i["priority"] == "low":
+            print(f"Low: {i["task"]}")
 
 if isinstance(option, int):
     if option == 1:
@@ -64,7 +85,11 @@ if isinstance(option, int):
         mark_Task() 
     elif option == 4:
         delete_Task()
+    elif option == 5:
+        view_incomplete()
+    elif option == 6:
+        view_priority()
 else:
-    option = input("Press 1 to add a task, Press 2 to view a taks, Press 3 to mark a task, Press 4 to Delete a task ")
+    option = input("Press 1 to add a task, 2 to view a taks, 3 to edit a task, 4 to Delete a task, 5 to view incomplete tasks, 6 to view priorities ")
 
 
